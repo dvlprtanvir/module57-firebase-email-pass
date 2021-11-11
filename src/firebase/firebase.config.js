249280b568922a3ec270057cde85194c18352dbb
Tanvir Hassan <dvlprtanvir@gmail.com>
2021-11-11 06:48:16 +0600

    done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD5qEPSuhziwS3g37qfGTs03GcgsF8hsZk",
+    authDomain: "module-57-email-pass.firebaseapp.com",
+    projectId: "module-57-email-pass",
+    storageBucket: "module-57-email-pass.appspot.com",
+    messagingSenderId: "522035605861",
+    appId: "1:522035605861:web:58e047990fe2aabbd2718c"
+  };
+
+  export default firebaseConfig;
